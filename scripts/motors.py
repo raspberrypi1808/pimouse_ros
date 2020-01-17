8e65ab70a1cf75a2656e856a5ced3fcc19e1190d
takata okada <aozoratakata@gmail.com>
2020-01-17 23:08:43 +0900

    Add copyright and license

--- scripts/motors.py
+++ scripts/motors.py
@@ -3,8 +3,8 @@
 import sys, rospy, math
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
-from std_srvs.srv import Trigger, TriggerResponse                               
-from pimouse_ros.srv import TimedMotion  
+from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.srv import TimedMotion                                               #追加
 
 class Motor():
     def __init__(self):
@@ -13,8 +13,9 @@ class Motor():
         rospy.on_shutdown(self.set_power)
         self.sub_raw = rospy.Subscriber('motor_raw', MotorFreqs, self.callback_raw_freq)
         self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
-        self.srv_on = rospy.Service('motor_on', Trigger, self.callback_on)            #追加
-        self.srv_off = rospy.Service('motor_off', Trigger, self.callback_off)         #追加
+        self.srv_on = rospy.Service('motor_on', Trigger, self.callback_on)
+        self.srv_off = rospy.Service('motor_off', Trigger, self.callback_off)
+        self.srv_tm = rospy.Service('timed_motion', TimedMotion, self.callback_tm)    #追加
         self.last_time = rospy.Time.now()
         self.using_cmd_vel = False
 
@@ -54,7 +55,7 @@ class Motor():
         self.using_cmd_vel = True
         self.last_time = rospy.Time.now()
 
-    def onoff_response(self,onoff):                                #以下3つのメ ソッドを追加
+    def onoff_response(self,onoff):                                #以下3つのメソッドを追加
         d = TriggerResponse()
         d.success = self.set_power(onoff)
         d.message = "ON" if self.is_on else "OFF"
@@ -62,6 +63,22 @@ class Motor():
 
     def callback_on(self,message): return self.onoff_response(True)
     def callback_off(self,message): return self.onoff_response(False)
+
+    def callback_tm(self,message):
+        if not self.is_on:
+            rospy.logerr("not enpowered")
+            return False
+
+        dev = "/dev/rtmotor0"
+        try:
+            with open(dev,'w') as f:
+                f.write("%d %d %d\n" %
+                    (message.left_hz,message.right_hz,message.duration_ms))
+        except:
+            rospy.logerr("cannot write to " + dev)
+            return False
+
+        return True
 
 if __name__ == '__main__':
     rospy.init_node('motors')
